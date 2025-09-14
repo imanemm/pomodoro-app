d6a91acfdab50e7a00208ffc7ee9fc69639ed9a4
imanemm <imanemoussamarou@gmail.com>
2025-09-13 22:02:58 -0400

    one time creation db

--- frontend/api.js
+++ frontend/api.js
@@ -1,4 +1,4 @@
-import {API_BASE} from "./config.js";
+const API_BASE = window.APP_CONFIG.API_BASE;
 
 export const logSession = async ({ durationSec, kind}) => {
     const res = await fetch(`${API_BASE}/api/sessions`, {
--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-export const API_BASE = "http://127.0.0.1:5000";
+window.APP_CONFIG = { API_BASE: "https://pomodoro-app-n5jf.onrender.com" };
 
